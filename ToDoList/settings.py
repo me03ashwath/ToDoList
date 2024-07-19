7692b4307247b6e0cb5fd50c394957b8b0418a44
Ashwath S <99113851+me03ashwath@users.noreply.github.com>
2024-07-19 10:43:01 +0530

    updated ALLOWED_HOSTS

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-q5j)03__w*h34pswmm89ejv_zyn!s4h6oqs2d_$q9g3196cqgy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.48.46.149']
 
 
 # Application definition
