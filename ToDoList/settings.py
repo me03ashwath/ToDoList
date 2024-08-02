7277b682f74b1c919a733a2413e0098d161d81a7
Ashwath S <99113851+me03ashwath@users.noreply.github.com>
2024-08-02 21:38:19 +0530

    static files configured

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -59,6 +60,7 @@ TEMPLATES = [
 ]
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
